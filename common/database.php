87c3ecf951960e3ac633f3ef5a6d185da3dc39f6
siniy <nanimonodemonai@gmail.com>
2022-08-14 11:44:56 +0900

    ユーザー登録機能を実装

--- common/database.php
+++ common/database.php
@@ -7,7 +7,7 @@
 function getDatabaseConnection() {
     try
     {
-        $database_handler = new PDO('mysql:host=bd;bdname=simple_memo;charset=utf8mb4','root','password');
+        $database_handler = new PDO('mysql:host=db;dbname=simple_memo;charset=utf8mb4','root','password');
     }
     catch(PDOException $e)
     {
